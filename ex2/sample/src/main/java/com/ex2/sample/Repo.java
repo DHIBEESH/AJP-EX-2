1231462bc701532cad4ecedf56159104ca4c8ac5
DHIBEESH <144550642+DHIBEESH@users.noreply.github.com>
2023-10-27 14:54:31 +0530

    Add files via upload

---
+++ ex2/sample/src/main/java/com/ex2/sample/Repo.java
@@ -0,0 +1,4 @@
+package com.ex2.sample;
+import org.springframework.data.repository.CrudRepository;
+public interface Repo extends CrudRepository<User,Integer>{
+} 
---
+++ ex2/sample/src/main/java/com/ex2/sample/User.java
@@ -0,0 +1,20 @@
+package com.ex2.sample;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+     @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
+  private String name;
+  private int age; 
+  private String gname;
+}
